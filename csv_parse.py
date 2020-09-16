cdac9aeab28daa525fa8ebd5dd66584762e15920
Chandler Scott <scottcha@mail.gvsu.edu>
2020-09-16 18:41:10 -0400

    attempting to search for careers page and continue spidering urls

--- csv_parse.py
+++ csv_parse.py
@@ -3,6 +3,9 @@ import requests
 # an array of url's we are gonna check
 targets = []
 sitemaps = []
+careers = []
+urls = []
+
 # go thru the member data line by line and get all the urls
 with open( 'members.csv', 'r' ) as f:
     for line in f:
@@ -37,7 +40,18 @@ for sitemap in sitemaps:
         text = text.split('\n')
         for t in text:
             if "http" in t:
-                print(t)
+                urls.append[t]
+                for url in urls:
+                    if 'career' or 'job' in url:
+                        careers.append[url]
+                        print(careers)
+                try:
+                    new_res = requests.get(t)
+                    soup2 = bs(res.text, 'lxml')
+                    text2 = soup.get_text()
+                    text2 = text2.split('\n')
+                except:
+                    continue
     except:
         continue
 
